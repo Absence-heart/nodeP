a1bd46ab4eb8eda9b95f3061c0e37206d3e93f3d
yhf <absence.heart@qq.com>
2018-11-08 21:00:05 +0800

    完善
    继续完善

--- practise/index.js
+++
@@ -1,5 +0,0 @@
-// console.log(arguments);
-// console.log(__dirname);
-// console.log(require);
-// console.log(__filename);
-console.log(Array.from(arguments));
--- practise/test.js
+++
@@ -1,5 +0,0 @@
-// require("./index.js");
-// module.exports = {a: "shabi"}
-var fs = require("fs");
-var data = fs.readFileSync(__dirname + "/test.conf").toString();
-console.log(data);
