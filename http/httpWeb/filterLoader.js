1731ba01f6fe1be318cad12a6a9f2db8aab801d2
yhf <absence.heart@qq.com>
2018-11-08 20:58:02 +0800

    完善
    继续完善

--- http/httpWeb/filterLoader.js
+++ http/httpWeb/filterLoader.js
@@ -1,17 +1,11 @@
 const fs = require('fs');
 const globalConf = require("./config");
 const filterSet = [];
-const files = fs.readdirSync(__dirname + "/" + globalConf["web_path"]);
+const files = fs.readdirSync(__dirname + "/" + globalConf["filter_path"]);
 for (let i = 0; i < files.length; i++ ) {
     const temp = require("./" + globalConf["web_path"] + "/" + files[i]);
     filterSet.push(temp);
-    // 避免web文件含有未配置的path
-    if(temp.path) {
-        for (const [k, v] of temp.path) {
-            pathMap.set(k, v);
-            kArr.push(k);
-        }
-    }
+
 }
 
 module.exports = pathMap;
